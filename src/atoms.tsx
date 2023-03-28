e92043553776609fb1e0b81f2ce7febe95491441
taehyeon0412 <71374539+taehyeon0412@users.noreply.github.com>
2023-03-29 01:11:29 +0900

    recoil Atom Effects
    recoil Atom Effects을 이용하여 로컬스토리지와 동기화

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -10,6 +10,21 @@ interface IToDoState {
 }
 //[key: string] 객체의 키가 문자열임을 의미합니다
 
+const localStorageEffect =
+  (key: string) =>
+  ({ setSelf, onSet }: any) => {
+    const savedData = localStorage.getItem(key);
+    if (savedData != null) {
+      setSelf(JSON.parse(savedData));
+    }
+    onSet((newValue: IToDoState) => {
+      localStorage.setItem(key, JSON.stringify(newValue));
+    });
+  };
+//localStorage 저장,불러오기
+/* 이 함수는 원자의 값을 업데이트하는 데 사용되는 setSelf와 
+원자의 값이 변경될 때마다 호출되는 onSet의 두 가지 인수를 받습니다. */
+
 export const toDoState = atom<IToDoState>({
   key: "toDo",
   default: {
@@ -20,6 +35,7 @@ export const toDoState = atom<IToDoState>({
     "하고 있는 일": [],
     "끝 마친 일": [{ id: 2, text: "보드나 카드를 추가해 보세요!" }],
   },
+  effects: [localStorageEffect(`saved-To-Do-data`)],
 });
 
 //"To Do"에만 ""있는 이유 => 띄어쓰기가 안되기때문에 ""로 감싸면 띄어쓰기 가능
